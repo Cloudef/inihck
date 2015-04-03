a7e375019d5ef55198cdc1bd87a8485b2802c26c
Jari Vetoniemi <mailroxas@gmail.com>
2015-04-03 17:30:56 +0300

    Fix fuzz.sh

--- src/fuzz.sh
+++ src/fuzz.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-true; while (( $? == 0 )); do s=$RANDOM; zzuf -U 60 -M -1 -s $s valgrind --error-exitcode=1 --leak-check=full ../test/fuzz_test; echo "iteration: $((i++))"; done; echo "failed on seed: $s"
+true; while (( $? == 0 )); do s=$RANDOM; echo "iteration $((++i))"; zzuf -x -U 60 -M -1 -s $s valgrind --error-exitcode=1 --leak-check=full ../test/fuzz_test; done; echo "failed on seed: $s"
